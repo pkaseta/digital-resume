37f4b0c3c7fb74ccf45e135da433432df856a178
Paul Kaseta Jr <kasetapaul@gmail.com>
2022-07-10 18:22:09 -0400

    added styling and structure for the discipline pages

--- src/Pages/DevDisciplinedataPage/DevDisciplineDataPage.js
+++ src/Pages/DevDisciplinedataPage/DevDisciplineDataPage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import DisciplineCard from "../../Components/DisciplineCards/DisciplineCard";
 import "../DevDisciplinedataPage/DevDisciplineDataPage.scss";
+
+//Image imports
+import EducationImg from "../../Data/education.jpeg";
+import ProjectsImg from "../../Data/projects.jpeg";
+import ExperienceImg from "../../Data/experience.jpeg";
 
 const DevDisciplineDataPage = () => {
   var pageSize = visualViewport.height - 85.38;
@@ -8,7 +14,21 @@ const DevDisciplineDataPage = () => {
       className="dev-discipline-container"
       style={{ height: `${pageSize}px`, margin: "0" }}
     >
-      DevDisciplineDataPage
+      <DisciplineCard
+        discipline={"Experience"}
+        bgImage={ExperienceImg}
+        clickHandler={() => console.log("Hi")}
+      />
+      <DisciplineCard
+        discipline={"Projects"}
+        bgImage={ProjectsImg}
+        clickHandler={() => console.log("Hi")}
+      />
+      <DisciplineCard
+        discipline={"Education"}
+        bgImage={EducationImg}
+        clickHandler={() => console.log("Hi")}
+      />
     </div>
   );
 };
